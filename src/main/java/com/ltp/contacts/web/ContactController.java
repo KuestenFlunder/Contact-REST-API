307ce54a3e22eb16d522d210c29e10d7a4ceec16
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-19 14:04:44 +0200

    first comit

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -9,7 +9,6 @@ import com.ltp.contacts.service.ContactService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
