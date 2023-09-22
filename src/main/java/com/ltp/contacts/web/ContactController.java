a125a2c8d09f80c6661fbeead6e5c82bd4520669
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-22 20:07:51 +0200

    Add Tags and Swagger Config

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -11,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,37 +22,40 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+//@Tag(name = "Contact Controller",description = "Create and retrieve contacts ")
 @RestController // Makes the @ResponseBody annotation obsolete for every method.
 public class ContactController {
 
     @Autowired
     private ContactService contactService;
 
+    @Tag(name= "GET contacts")
     @GetMapping("/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 
-    
+    //@Operation(summary = "Retrieves Contacts")
+    @Tag(name= "GET contacts")
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
-
+    @Tag(name= "POST new contact")
     @PostMapping("/contact")
     public ResponseEntity<HttpStatus> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
+ @Tag(name= "PUT contact update")
     @PutMapping("/contact/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable String id,@Valid @RequestBody Contact contact) {
         contactService.updateContact(id, contact);
         return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
     }
-
+ @Tag(name= "DELETE contact")
     @DeleteMapping("/contact/{id}")
     public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
         contactService.deleteContact(id);
