616dab6d0e79a77427dc34c0f42864f2a347c6ab
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-22 20:17:08 +0200

    add Operation summary and description

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -29,33 +29,40 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @Tag(name= "GET contacts")
+    @Operation(summary = "Retrieves Contacts", description = "Provides a list of all contacts")
+    @Tag(name = "GET contacts")
     @GetMapping("/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 
-    //@Operation(summary = "Retrieves Contacts")
-    @Tag(name= "GET contacts")
+    @Operation(summary = "Get contact by Id", description = "Returns a contact based on an ID")
+    @Tag(name = "GET contacts")
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
-    @Tag(name= "POST new contact")
+
+    @Tag(name = "POST new contact")
+    @Operation(summary = "Create Contact", description = "Creates a contact from the provided payload")
     @PostMapping("/contact")
     public ResponseEntity<HttpStatus> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
- @Tag(name= "PUT contact update")
+
+    @Tag(name = "PUT contact update")
+    @Operation(summary = "Update Contact", description = "Updates a contact based on the ID with the provided payload")
     @PutMapping("/contact/{id}")
-    public ResponseEntity<Contact> updateContact(@PathVariable String id,@Valid @RequestBody Contact contact) {
+    public ResponseEntity<Contact> updateContact(@PathVariable String id, @Valid @RequestBody Contact contact) {
         contactService.updateContact(id, contact);
         return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
     }
- @Tag(name= "DELETE contact")
+
+    @Tag(name = "DELETE contact")
+    @Operation(summary = "Delete Contact", description = "Delete contact based on the ID")
     @DeleteMapping("/contact/{id}")
     public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
         contactService.deleteContact(id);
