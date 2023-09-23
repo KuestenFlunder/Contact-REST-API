61009908c99f05dde2286620ffbd504c934da1fd
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-23 10:53:48 +0200

    prepare test data

--- src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -1,13 +1,31 @@
 package com.ltp.contacts;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ltp.contacts.pojo.Contact;
+import com.ltp.contacts.repository.ContactRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ContactsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@Autowired
+private MockMvc mockMvc;
+
+@Autowired
+private ContactRepository contactRepository;
+
+private Contact[] contacts = new Contact[]{
+new Contact("1","Peter Pan","123"),
+new Contact("2","Captain Hook","234"),
+new Contact("3","Tinker Bell","234"),
+};
+
+
+
 
 }
