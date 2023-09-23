595a5c7ef00dabb8f533a9187ec9acd527db81fa
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-23 10:58:32 +0200

    add before and after operations

--- src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.ltp.contacts;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,6 +26,19 @@ new Contact("1","Peter Pan","123"),
 new Contact("2","Captain Hook","234"),
 new Contact("3","Tinker Bell","234"),
 };
+
+@BeforeEach
+void setupTestRepository(){
+	for (Contact contact : contacts) {
+		contactRepository.saveContact(contact);
+	}
+}
+
+@AfterEach
+void clearTestRepository(){
+	contactRepository.getContacts().clear();
+}
+
 
 
 
