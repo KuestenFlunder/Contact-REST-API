bf4ecbc606e04845c7218263f0dd9abdde39fed9
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-23 11:46:43 +0200

    add GET request tests

--- src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -1,15 +1,24 @@
 package com.ltp.contacts;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
+
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.apache.catalina.connector.Request;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,7 +48,27 @@ void clearTestRepository(){
 	contactRepository.getContacts().clear();
 }
 
+@Test
+public void getContactByIdTest() throws Exception {
+	RequestBuilder request = MockMvcRequestBuilders.get("/contact/1");
+	
+	mockMvc.perform(request)
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.name").value(contacts[0].getName()))
+			.andExpect(jsonPath("$.phoneNumber").value(contacts[0].getPhoneNumber()));
+}
 
+
+@Test
+public void getAllContactsTest() throws Exception{
+	RequestBuilder request = MockMvcRequestBuilders.get("/contact/all");
+	mockMvc.perform(request)
+	.andExpect(status().isOk())
+	.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	.andExpect(jsonPath("$.size()").value(contacts.length))
+	;
+}
 
 
 
